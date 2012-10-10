2e83f4f1aa629db92ac157472844500ea134d469
matthieudelgado <matthieudelgado@192.168.1.105>
2012-10-10 12:11:25 +0200

    ajout de l'init de l'oid

--- Xml-Rmi/src/objets/Point.java
+++ Xml-Rmi/src/objets/Point.java
@@ -11,56 +11,66 @@ import xmlrmi.*;
 public class Point implements XMLRMISerializable, Stringable {
 
 	// attention construire une partie de l'XML en local pour les champs dans la metthode de contstruction XML
-	
-	
+
+
 	@XMLRMIField(serializationName = "x", serializationType = "double")
 	protected double a;
-	
+
 	@XMLRMIField(serializationName = "y", serializationType = "double")
 	protected double b;
 
 	@XMLRMIField(serializationName = "mark", serializationType = "string")
 	protected char marque='m';
-	
-	private String oid=this.getOid();
-	
+
+	private String oid = "";
+
+	private static Integer compteur = 0;
+
 	public Point(int x, int y){
 		this.a=x;
 		this.b=y;
 	}
-	
+
 	@Override
 	public String toString(){
 		return "x = "+this.a+ " y =  " + this.b;
 	}
-	
+
 	public String getOid(){
-		return "oidTest 007";
-	}
-	public void setOid(String oid){
-		this.oid=oid;
+		return this.oid;
 	}
 
-	// recuperer de l'interface la classe pour avoir les mŽthode a passer dans l'XML
+
+	// recuperer de l'interface la classe pour avoir les mï¿½thode a passer dans l'XML
 	@Override
 	public String toXML(Class<?> inter) {
+		initOid();
 		// TODO Auto-generated method stub
 		String interString = inter.getName();
-		
+
 		String tostring = "public String toString(){return \"x = \"+this.a+ \" y =  \" + this.b;}";
 		ArrayList<String> aString= new ArrayList<String>();
 		aString.add(tostring);
-		
+
 		ObjectToXML.docToFile((ObjectToXML.appelClientToDocument(this, "display", aString)),"data/appelClient.xml");
-		
-		
-		
+
+
+
 		return null;
 	}
 
 	@Override
 	public void updateFromXML(Element theXML) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void initOid() {
+		if(this.oid == "") return;
+		synchronized(compteur){
+			this.oid = "/"+this.getClass()+"/"+compteur;
+			compteur++;
+		}
 	}
 }
--- Xml-Rmi/src/objets/XMLRMISerializable.java
+++ Xml-Rmi/src/objets/XMLRMISerializable.java
@@ -4,4 +4,5 @@ public interface XMLRMISerializable {
 	
 	public String toXML(Class<?> inter) ; 
 	public void updateFromXML(org.w3c.dom.Element theXML) ;
+	public void initOid();
 }
