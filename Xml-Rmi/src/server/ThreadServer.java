3238584d8b2e42277ac955a4a7cd446f27800aaf
matthieudelgado <matthieudelgado@sylvain.home>
2012-10-13 18:23:36 +0200

    creation d'un methode xmltoobjectfrom node non testee execution de l'appel client par le serveur

--- Xml-Rmi/src/server/ThreadServer.java
+++ Xml-Rmi/src/server/ThreadServer.java
@@ -1,15 +1,24 @@
 package server;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.net.Socket;
+import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import objets.Stringable;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
 import tools.TestEcritureXML;
+import tools.XMLToObject;
 import xmlrmi.XMLInputStream;
 import xmlrmi.XMLOutputStream;
 
@@ -54,9 +63,66 @@ public class ThreadServer extends Thread implements IServer{
 		}
 	}
 
-	private void doTreatement(Document doc) {
+	private void doTreatement(Document doc) throws Exception {
 		TestEcritureXML.afficherDocument(doc);
+		//on commence par verifier l'integrite du doc par rapport a la grammaire
+		
+		//on recherche la méthode appelée
+		Node n = doc.getElementsByTagName("methodeName").item(0).getFirstChild();
+		String MethodeName = ((Text)n).getData();
+		//on recupère les parametres de la methode dans le xml
+		ArrayList<Node> paramList = new ArrayList<Node>();
+		NodeList params = doc.getElementsByTagName("value");
+		Node current = null;
+		for(int i = 0; i < params.getLength() ; i++) {
+			current = params.item(i);
+			if(current.getParentNode().getNodeName().equalsIgnoreCase("param"))
+				paramList.add(current.getFirstChild());
+		}
+		
+		
+		//on verfie qu'elle existe dans l'inteface IServer
+		Class<IServer> itf = IServer.class;
+		Method[] methods = itf.getDeclaredMethods();
+		Method calledMethod = null;
+		ArrayList<Object> args = new ArrayList<Object>();
+		Class<?>[] parameterTypes = null;
+		boolean trouve = false;
+		for(Method m : methods){
+			//on commence par verifier si le nom correspond
+			if( !m.getName().equalsIgnoreCase(MethodeName)) continue;
+			//on verifie ensuite les arguments
+			parameterTypes = m.getParameterTypes();
+			//on compare le type des parametres avec le xml
+			for(int i = 0 ; i < parameterTypes.length; i++){
+				System.err.println(parameterTypes[i].getSimpleName());
+				Object o = XMLToObject.createObjectFromNode(paramList.get(i));
+				if( ! parameterTypes[i].isInstance(o) ){
+					trouve = false;
+					break;
+				} else {
+					args.add(o);
+				}
+			}
+			if(trouve = false){
+				args.clear();
+				continue;
+			} else{
+				System.err.println("bla");
+				calledMethod = m;
+				break;
+			}
+			
+		}
+		
+		System.err.println(calledMethod.getName());
+		System.err.println(args.size());
+		Object ret = calledMethod.invoke(this, args.toArray());
+		
+		
+
 	}
+	
 
 	@Override
 	public void display(Stringable s) {
--- Xml-Rmi/src/tools/XMLToObject.java
+++ Xml-Rmi/src/tools/XMLToObject.java
@@ -1,6 +1,7 @@
 package tools;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -8,54 +9,127 @@ import javassist.CtClass;
 import javassist.CtField;
 import javassist.CtMethod;
 import javassist.CtNewMethod;
-import javassist.NotFoundException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
 public class XMLToObject {
 
 	private Document doc;
-	
+
 	public XMLToObject() {
 		this.doc=null;
 	}
-	
+
 	public Object createObject(Document doc) throws CannotCompileException, InstantiationException, IllegalAccessException{
-	
+
 		// on recupere le contenu de la balise method
 		String corpsMethode= doc.getElementsByTagName("method").item(0).getTextContent();
 		//String corpsMethode = "public String toString(){return \"r\";}";
 		String x = doc.getElementsByTagName("double").item(0).getTextContent();
 		String y = doc.getElementsByTagName("double").item(1).getTextContent();
-		
-		
+
+
 		System.out.println(corpsMethode);
-		
-			
-			 CtClass point = ClassPool.getDefault().makeClass("Point");
-			
-			CtField f  = new CtField(CtClass.doubleType,"x",point);
-			point.addField(f,x);
-			CtField f1  = new CtField(CtClass.charType,"mark",point);
-			point.addField(f1);
-			CtField f2  = new CtField(CtClass.doubleType,"y",point);
-			point.addField(f2,y);
-			
-			CtMethod m = CtNewMethod.make(corpsMethode, point);
-			point.addMethod(m);
-		
-	
-			Object p1 =point.toClass().newInstance();
-		
+
+
+		CtClass point = ClassPool.getDefault().makeClass("Point");
+
+		CtField f  = new CtField(CtClass.doubleType,"x",point);
+		point.addField(f,x);
+		CtField f1  = new CtField(CtClass.charType,"mark",point);
+		point.addField(f1);
+		CtField f2  = new CtField(CtClass.doubleType,"y",point);
+		point.addField(f2,y);
+
+		CtMethod m = CtNewMethod.make(corpsMethode, point);
+		point.addMethod(m);
+
+
+		Object p1 =point.toClass().newInstance();
+
 		return p1;
 	}
-	
+
+	public static Object createObjectFromNode(Node node) throws Exception{
+		if(!node.getParentNode().getNodeName().equalsIgnoreCase("value")) 
+			throw new Exception("le pere de l'objet n'est pas value"); 
+
+		if(node.getNodeName().equalsIgnoreCase("int")){
+			return Integer.parseInt(((Text)node.getFirstChild()).getData());
+		} else if(node.getNodeName().equalsIgnoreCase("double")){
+			return Double.parseDouble(((Text)node.getFirstChild()).getData());
+		} else if(node.getNodeName().equalsIgnoreCase("boolean")){
+			return Boolean.parseBoolean(((Text)node.getFirstChild()).getData());
+		} else if(node.getNodeName().equalsIgnoreCase("string")){
+			return new String(((Text)node.getFirstChild()).getData());
+		} else if(node.getNodeName().equalsIgnoreCase("dateTime")){
+			//TODO a completer
+		} else if(node.getNodeName().equalsIgnoreCase("base64")){
+			//TODO a completer
+		} else if(node.getNodeName().equalsIgnoreCase("array")){
+			ArrayList<Object> array = new ArrayList<Object>();
+			NodeList nl = node.getChildNodes();
+			for(int i = 0; i < nl.getLength(); i++){
+				array.add(createObjectFromNode(nl.item(i)));
+			}
+			return array;
+		} else if(node.getNodeName().equalsIgnoreCase("struct")){
+			//TODO a completer
+		} else if(node.getNodeName().equalsIgnoreCase("object")){
+			//on creer une hashmap pour garder la valeur des champs
+			HashMap<String, Object> fieldMap = new HashMap<String, Object>();
+			//on creer une classe avec pour nom l'oid de l'objet
+			String oid = node.getAttributes().getNamedItem("oid").getNodeValue();
+			CtClass clazz = ClassPool.getDefault().makeClass(oid);
+			NodeList nl = node.getChildNodes(), nl2;
+			Node current, n;
+			String name;
+			for(int i = 0; i< nl.getLength(); i++){
+				current = nl.item(i);
+				//on parcours les fields et les method
+				if(current.getNodeName().equalsIgnoreCase("fields")){
+					nl2 = current.getChildNodes();
+					for(int j = 0; j< nl2.getLength(); j++){
+						n = nl2.item(j);
+						//on ajoute chaque field dans la classe et on stoque la valeur dans la hashmap
+						if(!n.getNodeName().equals("field"))continue;
+						name = n.getAttributes().getNamedItem("name").getNodeValue();
+						CtField f  = new CtField(CtClass.voidType,name,clazz);
+						fieldMap.put(name, createObjectFromNode(n.getFirstChild().getFirstChild()));
+					}
+				} else if(current.getNodeName().equalsIgnoreCase("methods")){
+					//on ajoute la method a la classe
+					nl2 = current.getChildNodes();
+					for(int j = 0; j< nl2.getLength(); j++){
+						n = nl2.item(j);
+						if(!n.getNodeName().equals("method"))continue;
+						if(!n.getAttributes().getNamedItem("name").getNodeValue().equalsIgnoreCase("java"))continue;
+						CtMethod m = CtNewMethod.make(n.getTextContent(), clazz);
+						clazz.addMethod(m);
+					}
+				}
+			}
+			
+			//on initialise les champs de l'instance
+			Object o = clazz.toClass().newInstance();
+			for(String s : fieldMap.keySet()){
+				o.getClass().getDeclaredField(s).set(o, fieldMap.get(s));
+			}
+			return o;
+		} 
+
+		return null;
+	}
+
 	public void updateFromXml(Document doc,ArrayList<Object> lo){
 		String oid=doc.getElementsByTagName("object").item(0).getAttributes().getNamedItem("oid").getTextContent();
 		System.out.println(oid);
 		Object objToUpdate;
-		
+
 		// pour tout les objets, recupere la valeur de l'oid dans l'annotation
-		
+
 	}
 }
